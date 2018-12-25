92867139c2dd05671002a5e2e2b42f3fe8bc0a58
Sam Gehman <sgehman@cs.uw.edu>
2018-12-24 17:19:37 -0800

    Add code up to part 3

---
+++ detector/darknet.py
@@ -0,0 +1,226 @@
+from __future__ import division
+from util import *
+# import configparser
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+import numpy as np
+
+
+def get_test_input():
+    img = cv2.imread('data/dog-cycle-car.png')
+    img = cv2.resize(img, (416, 416))  # Resize to the input dimension
+    img_ = img[:, :, ::-1].transpose((2, 0, 1))  # BGR -> RGB | H X W C -> C X H X W
+    img_ = img_[np.newaxis, :, :, :] / 255.0  # Add a channel at 0 (for batch) | Normalise
+    img_ = torch.from_numpy(img_).float()  # Convert to float
+    img_ = Variable(img_)  # Convert to Variable
+    return img_
+
+
+def parse_cfg(cfgfile):
+    """
+    Parses a configuration file.
+
+    :param cfgfile: a configuration file
+    :return: a list of blocks. Each block describes a block in the neural network to be built. Block is represented as a
+    dictionary in the list.
+    """
+
+    file = open(cfgfile, 'r')
+    lines = file.read().split('\n')  # store the lines in a list
+    lines = [x for x in lines if len(x) > 0]  # get read of the empty lines
+    lines = [x for x in lines if x[0] != '#']  # get rid of comments
+    lines = [x.rstrip().lstrip() for x in lines]  # get rid of fringe whitespaces
+
+    block = {}
+    blocks = []
+
+    for line in lines:
+        if line[0] == "[":  # This marks the start of a new block
+            if len(block) != 0:  # If block is not empty, implies it is storing values of previous block.
+                blocks.append(block)  # add it the blocks list
+                block = {}  # re-init the block
+            block["type"] = line[1:-1].rstrip()
+        else:
+            key, value = line.split("=")
+            block[key.rstrip()] = value.lstrip()
+
+    blocks.append(block)
+    return blocks
+
+
+def create_modules(blocks):
+    net_info = blocks[0]  # Captures the information about the input and pre-processing
+    module_list = nn.ModuleList()
+    prev_filters = 3  # Initially corresponds to RGB channels
+    output_filters = []
+
+    for index, x in enumerate(blocks[1:]):
+        module = nn.Sequential()
+
+        if x["type"] == "convolutional":
+            # Get the info about the layer
+            activation = x["activation"]
+            try:
+                batch_normalize = int(x["batch_normalize"])
+                bias = False
+            except:
+                batch_normalize = 0
+                bias = True
+
+            filters = int(x["filters"])
+            padding = int(x["pad"])
+            kernel_size = int(x["size"])
+            stride = int(x["stride"])
+
+            if padding:
+                pad = (kernel_size - 1) // 2
+            else:
+                pad = 0
+
+            # Add the convolutional layer
+            conv = nn.Conv2d(prev_filters, filters, kernel_size, stride, pad, bias=bias)
+            module.add_module("conv_{0}".format(index), conv)
+
+            # Add the Batch Norm Layer
+            if batch_normalize:
+                bn = nn.BatchNorm2d(filters)
+                module.add_module("batch_norm_{0}".format(index), bn)
+
+            # Check the activation.
+            # It is either Linear or a Leaky ReLU for YOLO
+            if activation == "leaky":
+                activn = nn.LeakyReLU(0.1, inplace=True)
+                module.add_module("leaky_{0}".format(index), activn)
+
+        # If it's an upsampling layer
+        # We use Bilinear2dUpsampling
+        elif x["type"] == "upsample":
+            stride = int(x["stride"])
+            upsample = nn.Upsample(scale_factor=2, mode="bilinear")
+            module.add_module("upsample_{}".format(index), upsample)
+
+        # If it is a route layer
+        elif x["type"] == "route":
+            x["layers"] = x["layers"].split(',')
+            # Start  of a route
+            start = int(x["layers"][0])
+            # end, if there exists one.
+            try:
+                end = int(x["layers"][1])
+            except:
+                end = 0
+            # Positive annotation
+            if start > 0:
+                start = start - index
+            if end > 0:
+                end = end - index
+            route = EmptyLayer()
+            module.add_module("route_{0}".format(index), route)
+            if end < 0:
+                filters = output_filters[index + start] + output_filters[index + end]
+            else:
+                filters = output_filters[index + start]
+
+        # shortcut corresponds to skip connection
+        elif x["type"] == "shortcut":
+            shortcut = EmptyLayer()
+            module.add_module("shortcut_{}".format(index), shortcut)
+
+        # Yolo is the detection layer
+        elif x["type"] == "yolo":
+            mask = x["mask"].split(",")
+            mask = [int(x) for x in mask]
+
+            anchors = x["anchors"].split(",")
+            anchors = [int(a) for a in anchors]
+            anchors = [(anchors[i], anchors[i + 1]) for i in range(0, len(anchors), 2)]
+            anchors = [anchors[i] for i in mask]
+
+            detection = DetectionLayer(anchors)
+            module.add_module("Detection_{}".format(index), detection)
+
+        module_list.append(module)
+        prev_filters = filters
+        output_filters.append(filters)
+
+    return net_info, module_list
+
+
+class EmptyLayer(nn.Module):
+    def __init__(self):
+        super(EmptyLayer, self).__init__()
+
+
+class DetectionLayer(nn.Module):
+    def __init__(self, anchors):
+        super(DetectionLayer, self).__init__()
+        self.anchors = anchors
+
+
+class Darknet(nn.Module):
+    def __init__(self, cfgfile):
+        super(Darknet, self).__init__()
+        self.blocks = parse_cfg(cfgfile)
+        self.net_info, self.module_list = create_modules(self.blocks)
+
+    def forward(self, x, CUDA):
+        modules = self.blocks[1:]
+        outputs = {}  # Cache the outputs for the route layer
+        write = 0
+        for i, module in enumerate(modules):
+            module_type = (module["type"])
+            if module_type == "convolutional" or module_type == "upsample":
+                x = self.module_list[i](x)
+
+            elif module_type == "route":
+                layers = module["layers"]
+                layers = [int(a) for a in layers]
+
+                if (layers[0]) > 0:
+                    layers[0] = layers[0] - i
+
+                if len(layers) == 1:
+                    x = outputs[i + (layers[0])]
+
+                else:
+                    if (layers[1]) > 0:
+                        layers[1] = layers[1] - i
+
+                    map1 = outputs[i + layers[0]]
+                    map2 = outputs[i + layers[1]]
+
+                    x = torch.cat((map1, map2), 1)
+
+            elif module_type == "shortcut":
+                from_ = int(module["from"])
+                x = outputs[i - 1] + outputs[i + from_]
+
+            elif module_type == 'yolo':
+                anchors = self.module_list[i][0].anchors
+                # Get the input dimensions
+                inp_dim = int(self.net_info["height"])
+
+                # Get the number of classes
+                num_classes = int(module["classes"])
+
+                # Transform
+                x = x.data
+                x = predict_transform(x, inp_dim, anchors, num_classes, CUDA)
+                if not write:  # No collector has been initialized
+                    detections = x
+                    write = 1
+                else:
+                    detections = torch.cat((detections, x), 1)
+
+            outputs[i] = x
+
+        return detections
+
+
+model = Darknet('cfg/yolov3.cfg')
+inp = get_test_input()
+pred = model(inp, torch.cuda.is_available())
+print(pred)
